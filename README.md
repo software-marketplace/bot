dcfc1db3893430a1199fb8b703e8bd0e5efe9791
KryptoBlack <patrick.thakare123@gmail.com>
2023-02-17 08:00:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# software-marketplace-bot
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) that Software Marketplace Bot
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Docker
+
+```sh
+# 1. Build container
+docker build -t software-marketplace-bot .
+
+# 2. Start container
+docker run -e APP_ID=<app-id> -e PRIVATE_KEY=<pem-value> software-marketplace-bot
+```
+
+## Contributing
+
+If you have suggestions for how software-marketplace-bot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2023 KryptoBlack
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { Probot } from "probot";
+
+export = (app: Probot) => {
+    app.on("issues.opened", async (context) => {
+        const issueComment = context.issue({
+            body: "Thanks for opening this issue!",
+        });
+        await context.octokit.issues.createComment(issueComment);
+    });
+
+    app.on("push", async (context) => {
+        // get config file
+        const response: any = await context.octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+            owner: context.payload.repository.owner.login,
+            repo: context.payload.repository.name,
+            path: "/.marketplace.yaml"
+        })
+        if (response.status === 200) {
+            const data = {
+                config: JSON.parse(Buffer.from(response.data.content, 'base64').toString('utf-8')),
+                builtBy: context.payload.repository.owner.login,
+                repository: context.payload.repository.name
+            }
+
+            // context.log.debug(response.data.content)
+            context.log.debug(JSON.stringify(data))
+            // const res = await fetch('https://dev-smp.loca.lt/webhooks/config', {
+            const res = await fetch('http://localhost:8000/webhooks/config', {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            context.log.debug(res.status.toString())
+            // context.log.debug(response.data.content)
+        }
+        // app.log.debug("out")
+        // context.log.debug(configFile)
+    });
+    // For more information on building apps:
+    // https://probot.github.io/docs/
+
+    // To get your app running against GitHub, see:
+    // https://probot.github.io/docs/development/
+};
